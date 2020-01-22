9fe10af3dbacce6a21f442b0946106d1764bec46
Hae-gun <newchk610@gmail.com>
2020-01-22 18:47:56 +0900

    특강

---
+++ Web/200106/Algorithm/src/Test01.java
@@ -0,0 +1,13 @@
+
+public class Test01 {
+
+	public static void main(String[] args) {
+		
+		test a = new test();
+		test c = new test();
+		int b=c.solution("aaaccc");
+		System.out.println(b);
+		
+	}
+
+}
