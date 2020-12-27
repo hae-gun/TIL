145da6869a13759de12bd6e23f4ad992463ad6f9
Hae-Gun <newchk610@gmail.com>
2020-12-27 22:46:31 +0900

    boot

--- Language/JSP/Coupang/src/coupang/Ocean.java
+++ Language/JSP/Coupang/src/coupang/Ocean.java
@@ -1,5 +1,36 @@
 package coupang;
 
 public class Ocean {
-
+	public static void main(String[] args) {
+		
+	int[] dx = {1,0,-1,0};
+	int[] dy = {0,1,0,-1};
+	
+	boolean[][] visit = new boolean[5][4];
+	
+	int[][] ocean = { 
+			{1,1,1,0,0},
+			{0,0,1,0,0},
+			{0,0,1,0,0},
+			{0,0,0,0,0},
+			{1,1,0,0,1}
+			};
+		
+		for(int i=0; i<ocean.length; i++) {
+			for(int j=0; j<ocean[i].length; j++) {
+				
+				if(ocean[i][j] == 1 && !visit[i][j]) {
+					
+				}
+				
+				
+			}
+			
+			
+			
+		}
+	
+	
+		
+	}
 }
