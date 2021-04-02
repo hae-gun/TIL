95aade36f7d2b49a226620fe0963e6cb4d0fc683
Hae-Gun <newchk610@gmail.com>
2021-04-03 00:41:57 +0900

    refactory

--- CS/CT/Algorithm/SWExpert/src/normal/CleanRobot.java
+++ CS/CT/Algorithm/SWExpert/src/normal/CleanRobot.java
@@ -6,7 +6,6 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
 import java.util.stream.Collectors;
 
 public class CleanRobot {
@@ -69,12 +68,26 @@ public class CleanRobot {
 				count++;
 			}
 			
-			Cleaner next = new Cleaner();
+//			Cleaner next = new Cleaner();
+			Cleaner next = new Cleaner(cur.x, cur.y, cur.dir);
+			
 			
 			for(int dir = 0; dir < 4; dir++) {
-				next.dir = (cur.dir + 3 - dir) %4 ;
-				next.x = cur.x +  dx[ next.dir ];
-				next.y = cur.y +  dy[ next.dir ];
+				/********************************************
+				  
+				 *   이전코드.
+				 *   next.x += dx[ (next.dir + 3 - dir) %4  ];
+				 *   next.y += dy[ (next.dir + 3 - dir) %4  ];
+				 *   위 코드의 오류 
+				 *  
+				 *   방향 위치의 기준을 next로 잡음.
+				 *   즉 이미 로봇이 이동한 후에 그 위치에서 왼쪽을 보게됨.
+				 *   또한 next.dir을 갱신하지 않아 방향이 계속 초기 방향에서 탐색을 시작함.
+				
+				*******************************************/
+				next.dir = (cur.dir + 3 - dir) %4; // 해당 부분을 안해줘서 클리너의 방향이 계속 초기의 cur.dir로 고정되어 움직임.
+				next.x = cur.x +  dx[ next.dir ]; // 해당 부분을 안해줘서 위치가 계속 바뀜. cur.x 에서 next위치를 체크해야한다.
+				next.y = cur.y +  dy[ next.dir ]; 
 				
 				if(next.x < 0 || next.x >= map.length || next.y < 0 || next.y > map[0].length || map[next.x][next.y] != 0) continue;
 				que.add(next);
