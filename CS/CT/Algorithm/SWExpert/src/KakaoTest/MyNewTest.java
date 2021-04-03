bcb46db8cc972233cebae8b05985d53d1d7d0b3a
Hae-Gun <newchk610@gmail.com>
2021-04-03 12:51:33 +0900

    test

--- CS/CT/Algorithm/SWExpert/src/KakaoTest/MyNewTest.java
+++ CS/CT/Algorithm/SWExpert/src/KakaoTest/MyNewTest.java
@@ -2,8 +2,5 @@ package KakaoTest;
 
 public class MyNewTest {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
-		System.out.println("Hello World!");
-		System.out.println("Hello World!");
 	}
 }
